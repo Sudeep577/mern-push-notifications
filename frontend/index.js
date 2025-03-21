25384fe9c04f3d29d84ed79df5b3891bcf5b78a1
sudeep <sudeepkalpure@gmail.com>
2025-03-21 16:23:17 +0530

    branch testing

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,7 @@
-/**
- * @format
- */
+
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src/app.web';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
---
+++ frontend/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './index.web.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+  resolve: {
+    alias: {
+      'react-native$': 'react-native-web'
+    },
+    extensions: ['.web.js', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, 'public'),
+    compress: true,
+    port: 3000,
+  }
+};
